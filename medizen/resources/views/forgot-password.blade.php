b9de516ef332de05fa8bfc553128903f7821aaed
Chudasama Vishal <118047155+VishalChudasama08@users.noreply.github.com>
2026-04-29 12:02:16 +0530

    forgot password in same system only

--- medizen/resources/views/forgot-password.blade.php
+++ medizen/resources/views/forgot-password.blade.php
@@ -4,6 +4,8 @@
     <div class="container my-5 h-100">
         <div class="appointment-wrapper position-relative d-center w-100">
             <div style="height: 750px">
+                <p style="color:red;border:2px solid black;margin:10px;">This site run locally. (Not from a remote server)
+                    So, the reset link will be working in same system only.</p>
                 <form method="POST" action="/forgot-password" class="appointment-forms">
                     @csrf
                     <div class="row g-lg-3 g-3">
